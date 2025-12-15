44ddf7a71a96a8d96fd188404ef4f6dcb7592c17
Mohishaik1 <shaikmohddinsharieff@gmail.com>
2025-12-15 06:09:53 +0000

    Styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+//Pages
+import Navbar from './Components/Navbar.jsx';
 
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Navbar />
+    <h1>This is my portfolio</h1>
+    <p>Welcome to my personal portfolio website where I showcase my projects and skills.
+      My name is, Mohiddin Sharieff, and I am a, Product Developer.
+    Feel free to explore and get in touch!
+    </p>
+    
     </>
   )
 }
---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+//Stylea
+import '../Styles/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Services</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
